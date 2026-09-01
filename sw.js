f127c3964c789e87eb87cc950a71d64facba2811
jasminegloryfruit <92681925+jasminegloryfruit@users.noreply.github.com>
2026-09-01 23:59:11 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-const CACHE='money-tracker-v21';
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
